0c584fe2d2236ac06da9dc2c078106a8260d2023
Chen Reuven <chen.rvn@gmail.com>
2020-05-31 17:47:23 +0300

    add packages folder and code

--- workspace/packages/package1/index.js
+++ workspace/packages/package1/index.js
@@ -1,1 +1,7 @@
-console.log("Package 1 starting...");
+const double = (num) => num * 2
+const greet = () => console.log("Hello From Package 1")
+
+module.exports = {
+    double,
+    greet
+}
--- workspace/packages/package2/index.js
+++ workspace/packages/package2/index.js
@@ -1,1 +1,9 @@
 console.log("Package 2 starting...");
+
+const {
+    double,
+    greet
+} = require("package1");
+
+console.log(double(2));
+console.log(greet());
